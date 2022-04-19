1f3c1292d3eab78b1cf5e0986eee0fba1af5fd99
Rus1130 <arirogozi@gmail.com>
2022-04-19 15:57:14 -0400

    commit

--- misc/array-game/array-game.js
+++ misc/array-game/array-game.js
@@ -1,73 +1,134 @@
 const ArrayGame = {
-    bkgd: "#",
+    settings: {
+        bkgd: "#",
+        consoleWarns: true
+    },
+    gameboardX: 0,
+    gameboardY: 0,
+    setup: true,
+    do_not_edit: {
+        random(min, max) {
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            return Math.floor(Math.random() * (max - min + 1) + min);
+        },
+        randomPlayerState: [{
+            value: "up",
+            probability: 0.25
+        },{
+            value: "left",
+            probability: 0.25
+        },{
+            value: "down",
+            probability: 0.25
+        },{
+            value: "right",
+            probability: 0.25
+        
+        }],
+        randomizer: (values) => {
+            let i, pickedValue, 
+            randomNr = Math.random(),
+            threshold = 0;
+            for (i = 0; i < values.length; i++) {
+                if (values[i].probability === '*'){ 
+                    continue;
+                }
+                threshold += values[i].probability; 
+                if(threshold > randomNr) {
+                    pickedValue = values[i].value;
+                    break;
+                }
+                if (!pickedValue) {
+                    pickedValue = values.filter((value) => value.probability === '*');
+                }
+            }
+            return pickedValue;
+        },
+    },
     gameboard: [],
-    presetBoard: function(screenID, board_X, board_Y){
+    presetBoard: function(screenID, gameboard_x, gameboard_y){
         ArrayGame.display.create.screen(screenID);
-        ArrayGame.display.create.board(board_X, board_Y);
-        ArrayGame.display.update()
+        ArrayGame.display.create.board(gameboard_x, gameboard_y);
+        ArrayGame.display.setup()
+        if(ArrayGame.settings.consoleWarns) console.warn("Preset Board Created")
+        return ArrayGame.gameboard;
 
     },
     display: {
         create : {
-            screen: function(elementID) {
+            screen: function(screenID) {
                 let newGameboardDisplay = document.createElement("div");
                 newGameboardDisplay.id = "screen";
                 newGameboardDisplay.style.fontFamily = "monospace";
                 newGameboardDisplay.style.fontSize = "18px";
-                document.getElementById(elementID).appendChild(newGameboardDisplay);
+                document.getElementById(screenID).appendChild(newGameboardDisplay);
+                if(ArrayGame.settings.consoleWarns) return console.warn("Screen Created")
             },
             board: function (gameboard_x, gameboard_y) {
                 if(gameboard_x > 64 || gameboard_y > 64) {
                     return console.error("Gameboard size is too large (maximum is 64x64)")
                 } else {
-                    if(JSON.stringify(ArrayGame.gameboard) != "[]") {
-                        return console.error("Gameboard already exists; call ArrayGame.gameboard.destroy() first.")
-                    } else {
-                        for (i = 0; i < parseInt(gameboard_y); i++) {
-                            ArrayGame.gameboard.push([]);
-                            for (j = 0; j < parseInt(gameboard_x); j++) {
-                                ArrayGame.gameboard[i].push(ArrayGame.bkgd);
-                            }
+                    ArrayGame.gameboard = []
+                    ArrayGame.gameboardX = gameboard_x;
+                    ArrayGame.gameboardY = gameboard_y;
+                    for (i = 0; i < parseInt(gameboard_y); i++) {
+                        ArrayGame.gameboard.push([]);
+                        for (j = 0; j < parseInt(gameboard_x); j++) {
+                            ArrayGame.gameboard[i].push(ArrayGame.settings.bkgd);
                         }
-                        return ArrayGame.gameboard
-                    };
+                    }
+                    if(ArrayGame.settings.consoleWarns) console.warn("Gameboard Created")
+                    return ArrayGame.gameboard
                 };
             },
         },
-        update: function () {
+        setup: function () {
+            let playerSetupState = ArrayGame.do_not_edit.randomizer(ArrayGame.do_not_edit.randomPlayerState)
+            ArrayGame.entities.player.state = playerSetupState
+            ArrayGame.entities.player.sprite = ArrayGame.entities.player.states[playerSetupState]
+
+            let newPlayerX = ArrayGame.do_not_edit.random(0, ArrayGame.gameboardX - 1)
+            let newPlayerY = ArrayGame.do_not_edit.random(0, ArrayGame.gameboardY - 1)
+
+            ArrayGame.entities.player.x = newPlayerX
+            ArrayGame.entities.player.y = newPlayerY
+
+            ArrayGame.gameboard[newPlayerY][newPlayerX] = ArrayGame.entities.player.sprite
+
             document.getElementById("screen").innerHTML = JSON.stringify(ArrayGame.gameboard).replaceAll("],[","<br>").replace("[[","").replace("]]","").replaceAll('"',"").replaceAll(",","")
+            if(ArrayGame.settings.consoleWarns) return console.warn("Board Setup Completed")
             
         },
-        destroy: {
-            board: function () {
-                ArrayGame.gameboard = [];
-                return ArrayGame.gameboard
-            }
+        update: function () {
+            document.getElementById("screen").innerHTML = JSON.stringify(ArrayGame.gameboard).replaceAll("],[","<br>").replace("[[","").replace("]]","").replaceAll('"',"").replaceAll(",","")
+            if(ArrayGame.settings.consoleWarns) return console.warn("Board Updated")
+            
         }
     },
-    system: {
-        entities: {
-            sprites: {
-                player: {
-                    up: "╧",
-                    down: "╤",
-                    left: "╢", 
-                    right: "╟"
-                },
-                enemy: "",
-                bullet: {
-                    up: "|",
-                    down: "|",
-                    left: "-",
-                    right: "-"
-                }
-            },
-            player: {
-                x: 0,
-                y: 0,
-            },
-            enemies: [],
-            bullets: [],
-        }
+    entities: {
+        player: {
+            x: 0,
+            y: 0,
+            sprite: "",
+            state: "",
+            states: {
+                up: "╧",
+                down: "╤",
+                left: "╢", 
+                right: "╟"
+            }
+        },
+        bullet: {
+            state: "",
+            states: {
+                up: "|",
+                down: "|",
+                left: "-",
+                right: "-"
+            }
+        },
+        enemies: [],
+        bullets: [],
     }
 }
