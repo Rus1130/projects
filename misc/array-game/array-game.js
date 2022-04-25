b3d124a502709fdf3f4fd90c8dc3cdb4ecfc658c
Rus1130 <arirogozi@gmail.com>
2022-04-25 13:20:25 -0400

    commit

--- misc/array-game/array-game.js
+++ misc/array-game/array-game.js
@@ -1,10 +1,38 @@
 const ArrayGame = {
+    entities: {
+        player: {
+            x: 0,
+            y: 0,
+            sprite: "",
+            state: "",
+        },
+        bullet: {
+            state: "",
+            sprite: "",
+        },
+        enemies: [],
+        bullets: [],
+    },
     settings: {
         bkgd: "#",
-        consoleWarns: true
+        consoleWarns: true,
+        game_state: 'start-up'
     },
-    setup: true,
     DNE: {
+        stateTypes: {
+            player : {
+                up: "╧",
+                down: "╤",
+                left: "╢", 
+                right: "╟"
+            },
+            bullet: {
+                up: "|",
+                down: "|",
+                left: "-",
+                right: "-"
+            }
+        },
         random(min, max) {
             min = Math.ceil(min);
             max = Math.floor(max);
@@ -60,8 +88,36 @@ const ArrayGame = {
 
     },
     display: {
+        movePlayer: {
+            up: function(cellAmount){
+                ArrayGame.entities.player.state = "up"
+                if(ArrayGame.entities.player.y > 0){
+                    ArrayGame.entities.player.y -= cellAmount
+                }
+            },
+            down: function(cellAmount){
+                ArrayGame.entities.player.state = "down"
+                if(ArrayGame.entities.player.y < ArrayGame.gameboard.length - 1){
+                    ArrayGame.entities.player.y += cellAmount
+                }
+            },
+            left: function(cellAmount){
+                ArrayGame.entities.player.state = "left"
+                if(ArrayGame.entities.player.x > 0){
+                    ArrayGame.entities.player.x -= cellAmount
+                }
+
+            },
+            right: function(cellAmount){
+                ArrayGame.entities.player.state = "right"
+                if(ArrayGame.entities.player.x <  ArrayGame.gameboard[0].length - 1){
+                    ArrayGame.entities.player.x += cellAmount
+                }
+            }
+        },
         create : {
             screen: function(screenID) {
+                ArrayGame.settings.game_state = "screen_created"
                 let newGameboardDisplay = document.createElement("div");
                 newGameboardDisplay.id = "screen";
                 newGameboardDisplay.style.fontFamily = "monospace";
@@ -73,6 +129,7 @@ const ArrayGame = {
                 if(gameboard_x > 64 || gameboard_y > 64) {
                     return console.error("Gameboard size is too large (maximum is 64x64)")
                 } else {
+                    if(ArrayGame.settings.game_state !== "screen_created") return console.error("Screen has not been created yet")
                     ArrayGame.gameboard = []
                     ArrayGame.DNE.gameboardX = gameboard_x;
                     ArrayGame.DNE.gameboardY = gameboard_y;
@@ -81,65 +138,53 @@ const ArrayGame = {
                         for (j = 0; j < parseInt(gameboard_x); j++) {
                             ArrayGame.gameboard[i].push(ArrayGame.settings.bkgd);
                         }
-            
                     }
                     if(ArrayGame.settings.consoleWarns) console.warn("Gameboard Created")
+                    ArrayGame.settings.game_state = "board_created"
                     return ArrayGame.gameboard
                 };
             },
         },
         setup: function () {
+            if(ArrayGame.settings.game_state !== "board_created") return console.error("Gameboard has not been created yet")
             let playerSetupState = ArrayGame.DNE.randomizer(ArrayGame.DNE.randomPlayerState)
             ArrayGame.entities.player.state = playerSetupState
-            ArrayGame.entities.player.sprite = ArrayGame.entities.player.states[playerSetupState]
+            ArrayGame.entities.player.sprite = ArrayGame.DNE.stateTypes.player[playerSetupState]
 
-            let newPlayerX = ArrayGame.DNE.random(0, ArrayGame.DNE.gameboardX - 1)
-            let newPlayerY = ArrayGame.DNE.invert(ArrayGame.DNE.gameboardY, ArrayGame.DNE.random(0, ArrayGame.DNE.gameboardY - 1))
+            let playerX = ArrayGame.DNE.random(0, ArrayGame.DNE.gameboardX - 1)
+            let playerY = ArrayGame.DNE.invert(ArrayGame.DNE.gameboardY, ArrayGame.DNE.random(0, ArrayGame.DNE.gameboardY - 1))
 
-            ArrayGame.entities.player.x = newPlayerX
-            ArrayGame.entities.player.y = newPlayerY
+            ArrayGame.entities.player.x = playerX
+            ArrayGame.entities.player.y = playerY
 
-            ArrayGame.gameboard[newPlayerY][newPlayerX] = ArrayGame.entities.player.sprite
+            ArrayGame.gameboard[playerY][playerX] = ArrayGame.entities.player.sprite
+
             for(k = 0; k < ArrayGame.gameboard[0].length; k++){
                 ArrayGame.DNE.straightFrame.push("═")
             }
             let topFrame = "╔" + ArrayGame.DNE.straightFrame.join("") + "╗"
             let bottomFrame = "╚" + ArrayGame.DNE.straightFrame.join("") + "╝"
+
             document.getElementById("screen").innerHTML = topFrame + "<br>" + JSON.stringify(ArrayGame.gameboard).replaceAll("],[","║<br>║").replace("[[","║").replace("]]","║").replaceAll('"',"").replaceAll(",","") + "<br>" + bottomFrame
+            ArrayGame.settings.game_state = "ready"
             if(ArrayGame.settings.consoleWarns) return console.warn("Board Setup Completed")
             
         },
         update: function () {
+            if(ArrayGame.settings.game_state !== "ready") return console.error("Game is not ready to update")
             let topFrame = "╔" + ArrayGame.DNE.straightFrame.join("") + "╗"
             let bottomFrame = "╚" + ArrayGame.DNE.straightFrame.join("") + "╝"
+        
+            for (i = 0; i < ArrayGame.gameboard.length; i++) {
+                for (j = 0; j < ArrayGame.gameboard[i].length; j++) {
+                    ArrayGame.gameboard[i][j] = ArrayGame.settings.bkgd;
+                }
+            }
+
+            ArrayGame.entities.player.sprite = ArrayGame.DNE.stateTypes.player[ArrayGame.entities.player.state]
+            ArrayGame.gameboard[ArrayGame.entities.player.y][ArrayGame.entities.player.x] = ArrayGame.entities.player.sprite
 
             document.getElementById("screen").innerHTML = topFrame + "<br>" + JSON.stringify(ArrayGame.gameboard).replaceAll("],[","║<br>║").replace("[[","║").replace("]]","║").replaceAll('"',"").replaceAll(",","")  + "<br>" + bottomFrame
-            if(ArrayGame.settings.consoleWarns) return console.warn("Board Updated")
         }
-    },
-    entities: {
-        player: {
-            x: 0,
-            y: 0,
-            sprite: "",
-            state: "",
-            states: {
-                up: "╧",
-                down: "╤",
-                left: "╢", 
-                right: "╟"
-            }
-        },
-        bullet: {
-            state: "",
-            states: {
-                up: "|",
-                down: "|",
-                left: "-",
-                right: "-"
-            }
-        },
-        enemies: [],
-        bullets: [],
     }
 }
