b616636b1ca93b9cc4f54c4cd30adc26db89774c
Rus1130 <arirogozi@gmail.com>
2023-09-11 13:29:53 -0400

    commit

--- bitram-hacker/main.js
+++
@@ -1,9 +0,0 @@
-function increaseHeat(amnt){
-    let counter = +document.getElementById('heat-counter').textContent.split(' ')[1].split('/')[0]
-    document.getElementById('heat-counter').textContent = `Heat: ${counter + amnt}/100`
-}
-
-function decreaseHeat(amnt){
-    let counter = +document.getElementById('heat-counter').textContent.split(' ')[1].split('/')[0]
-    document.getElementById('heat-counter').textContent = `Heat: ${counter - amnt}/100`
-}
