67e0e11b94a5857912326df1fd21433533cadf12
Rus1130 <arirogozi@gmail.com>
2024-03-01 12:49:03 -0500

    commit

--- .global/player.js
+++ .global/player.js
@@ -6,9 +6,26 @@ class Player {
         this.y = y;
         this.vx = 0;
         this.vy = 0;
+        this.inventory = {};
         this.overflowType = overflowType || 'restrict';
         document.addEventListener('keydown', function(event) { Player.keys[event.key] = true; });
         document.addEventListener('keyup', function(event) { Player.keys[event.key] = false; });
+    }
+
+    inventoryAdd(item, amount) {
+        if(this.inventory[item]) this.inventory[item] += amount;
+        else this.inventory[item] = amount;
+    }
+
+    inventoryRemove(item, amount) {
+        if(this.inventory[item]) this.inventory[item] -= amount;
+        else this.inventory[item] = 0;
+    }
+
+    initializeInventory(arr){
+        for(let i = 0; i < arr.length; i++){
+            this.inventoryAdd(arr[i], 0);
+        }
     }
 
     move() {
