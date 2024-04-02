c4d9c5292b28386fbb17dc318940d0e5bf8ce1e1
Rus1130 <arirogozi@gmail.com>
2024-04-02 07:41:06 -0400

    Create sols.py

---
+++ sols.py
@@ -0,0 +1,13 @@
+import pyautogui, time
+pyautogui.mouseInfo()
+time.sleep(5)
+
+
+
+# while True:
+#     pyautogui.click(2574, 897, duration = 1)
+#     pyautogui.click(2574, 987, duration = 1)
+#     if pyautogui.pixel(2574, 987) 
+#     pyautogui.click(2574, 1057, duration = 1)
+#     pyautogui.click(2574, 1137, duration = 1)
+#     pyautogui.click(2574, 1327, duration = 1)
