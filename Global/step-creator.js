c49d71a9082410f13ee30fdd2d41d224d7908e6b
Rus1130 <arirogozi@gmail.com>
2024-10-11 14:00:35 -0400

    e

--- Global/step-creator.js
+++ Global/step-creator.js
@@ -1,5 +1,6 @@
 class StepCreator {
-    static reveal(id){
+    static StepElements = [];
+    reveal(id){
         if(typeof id === "string") document.getElementById(id).style.display = "block";
         else if (typeof id === "object") {
             for (let i = 0; i < id.length; i++){
@@ -16,6 +17,23 @@ class StepCreator {
 
     addStep(json) {
         let step = document.createElement('div');
+        StepCreator.StepElements.push(step);
+
+        if(StepCreator.StepElements.length > 1) step.style.display = "none";
+        /** custom property stuff */
+        // if(json.type === "radio"){
+        //     inputs[i].addEventListener('click', function(){
+        //         let name = this.name;
+        //         let value = this.value;
+        //         let allInputs = document.getElementsByName(name);
+        //         for (let j = 0; j < allInputs.length; j++){
+        //             if (allInputs[j].value !== value){
+        //                 allInputs[j].disabled = true;
+        //             }
+        //         }
+        //     });
+        // }
+
         step.id = json._id;
         step.textContent = json._text;
         this.element.appendChild(step);
@@ -36,5 +54,27 @@ class StepNode {
     text(text){
         this._text = text;
         return this;
+    }
+    /**
+     * 
+     * @param {string} name the name of the radio group
+     * @param {string[]} values the internal values of the radio buttons
+     * @param {string[]} ids what the radio buttons will reveal when clicked
+     * @param {string[]} texts the display text of the radio buttons
+     */
+    addRadio(name, values, ids, texts){
+        if(this.type !== "radio") return console.error("Invalid type");
+        if(values.length !== ids.length || values.length !== texts.length) return console.error("length mismatch");
+
+        let container = document.getElementById(this._id);
+
+        for(let i = 0; i < values.length; i++){
+            let radio = document.createElement('input');
+            radio.type = "radio";
+            radio.name = name;
+            radio.value = values[i];
+            radio.oninput = this.reveal(ids[i]);
+            container.appendChild(radio);
+        }
     }
 }
