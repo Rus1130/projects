a289fb8a01f7f416f1996d02dc5f15fdaf7d19d1
Rus1130 <arirogozi@gmail.com>
2024-10-24 09:07:46 -0400

    ee

--- Global/typer.js
+++ Global/typer.js
@@ -11,46 +11,71 @@ class Typer {
 
     end with ¶
     */
+   /**
+    * 
+    * @param {String} inputID 
+    * @param {String} outputID 
+    * 
+    * @param {Object} options options object
+    * @param {String} options.newlineDelimiter delegates where to put a newline
+    * @param {String} options.breakDelimiter delegates where to put a break
+    * @param {String} options.italicize italicizes the next word
+    * @param {String} options.increaseDelayChar increases the delay
+    * @param {String} options.endChar ends the text
+    * @param {Number} options.newlineDelay delay in ms to wait before resuming typing
+    * @param {Number} options.breakDelay delay in ms to wait before resuming typing
+    * @param {Number} options.charDelay delay in ms to wait before typing next character
+    * @param {Object<string, number>} options.customDelays custom delays for specific characters
+    * @param {Function} options.onFinish fires after finishing typing
+    */
     constructor(inputID, outputID, options){
-        this.inputEl = document.getElementById(inputID);
-        this.outputEl = document.getElementById(outputID);
-        this.text = this.inputEl.innerText.split(options.breakDelimiter || '~');
-        this.index = 0;
-        this.line = 0;
-        this.charDelay = options.charDelay || 100;
+
+
         this.newlineDelimiter = options.newlineDelimiter || '_';
         this.breakDelimiter = options.breakDelimiter || '~';
+        this.italicize = options.italicize || '^';
+        this.increaseDelayChar = options.increaseDelayChar || '«';
+        this.endChar = options.endChar || '¶';
         this.newlineDelay = options.newlineDelay || 0;
         this.breakDelay = options.breakDelay || 0;
+
+        this.inputEl = document.getElementById(inputID);
+        this.outputEl = document.getElementById(outputID);
+        this.text = this.inputEl.innerText.split(this.breakDelimiter);
+        this.index = 0;
+        this.line = 0;
+
+        this.charDelay = options.charDelay || 100;
         this.customDelays = options.customDelays;
         this.onFinish = options.onFinish;
 
         this.italic = false;
+        this.paused = false;
     }
 
     start() {
         // if last character isnt ¶, return error
-        if(this.text[this.text.length - 1].slice(-1) != "¶") {
-            console.error("Last character of text must be ¶ (yes i could append it myself im just doing this to troll you)");
+        if(this.text[this.text.length - 1].slice(-1) != this.endChar) {
+            console.error(`Last character of text must be ${this.endChar} (yes i could append it myself im just doing this to troll you)`);
             return;
         }
 
-        window.scrollTo(0, document.body.scrollHeight);
+        window.scrollBy(0, document.body.scrollHeight);
         if (this.index < this.text[this.line].length) {
             let char = this.text[this.line][this.index];
 
-            if(char == "^") {
+            if(char == this.italicize) {
                 char = "";
                 this.italic = true;
             }
 
-            if(char == "«") {
+            if(char == this.increaseDelayChar) {
                 char = "";
                 this.charDelay = 110;
                 this.customDelays[","] = 750;
             }
 
-            if(char == "¶") {
+            if(char == this.endChar) {
                 this.finished = true;
                 this.onFinish();
                 return;
@@ -62,13 +87,13 @@ class Typer {
             if(char == this.newlineDelimiter){
                 this.outputEl.innerHTML += '<br>';
                 this.index++;
-                setTimeout(() => this.start(), this.newlineDelay);
+                if(!this.paused) setTimeout(() => this.start(), this.newlineDelay);
             } else {
                 let delay = this.customDelays[char] || this.charDelay;
                 if(this.italic) char = "<i>" + char + "</i>";
                 this.outputEl.innerHTML += char;
                 this.index++;
-                setTimeout(() => this.start(), delay);
+                if(!this.paused) setTimeout(() => this.start(), delay);
             }
         } else {
             this.index = 0;
@@ -77,15 +102,46 @@ class Typer {
                 setTimeout(() => {
                     this.outputEl.innerHTML += '<br><br>';
                     setTimeout(() => {
-                        this.start();
+                        if(!this.paused) this.start();
                     }, this.newlineDelay);
                 }, this.breakDelay);
             }
         }
     }
 
+    /**
+     * 
+     * @returns {Number} estimated time in ms to finish typing
+     */
+    getEstimatedTime(){
+        let time = 0;
+        for(let i = 0; i < this.text.length; i++){
+            for(let j = 0; j < this.text[i].length; j++){
+                let char = this.text[i][j];
+                let delay = this.customDelays[char] || this.charDelay;
+                time += delay;
+            }
+            time += this.newlineDelay;
+        }
+        return time;
+    }
     setSpeed(delay) {
         this.charDelay = delay;
+    }
+
+    /**
+     * Pauses the typer
+     */
+    pause() {
+        this.paused = true;
+    }
+
+    /**
+     * Resumes the typer
+     */
+    resume() {
+        this.paused = false;
+        this.start();
     }
 
 }
