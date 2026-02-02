30ff729f7c4d42e31887279d2c0baa505f59b402
Rus1130 <arirogozi@gmail.com>
2026-02-02 11:29:59 -0500

    gaga

--- Global/dropdowns.js
+++ Global/dropdowns.js
@@ -1,24 +1,128 @@
-class Dropdown {
+class Dropdown extends EventTarget {
+    static styleElement = null;
+    static CSS = `    .dropdown {
+        position: relative;
+        display: inline-block;
+        font-size: 12px;
+        font-family: "Arial";
+        background-color: #f0f0f0;
+        border: 1px solid #ccc;
+        padding: 6px 8px;
+        user-select: none;
+        cursor: pointer;
+    }
+
+    .dropdown-hover {
+        background-color: #e0e0e0;
+    }
+
+    .dropdown-options-container {
+        font-family: "Arial";
+        user-select: none;
+        padding: 4px 8px;
+        background-color: white;
+        border: 1px solid #ccc;
+        position: relative;
+        width: max-content;
+        white-space: nowrap;
+    }
+
+    .dropdown-options-container.hide {
+        display: none;
+    }
+
+    .dropdown-option {
+        font-size: 12px;
+        display: flex;
+        align-items: center;
+    }
+
+    .dropdown-checkbox {
+        cursor: pointer;
+    }`
+
+    static ensureStyle() {
+        if (!Dropdown.styleElement) {
+            let el = document.querySelector('style[data-dropdown-css]');
+            if (!el) {
+                const styleElement = document.createElement('style');
+                styleElement.dataset.dropdownCss = "true";
+                styleElement.textContent = Dropdown.CSS;
+                Dropdown.styleElement = styleElement;
+                document.head.appendChild(styleElement);
+            }
+            Dropdown.styleElement = el;
+        }
+
+        return Dropdown.styleElement;
+    }
+
+        
+    /**
+     * @param {string} selector
+     * @param {Object<string,string|number>} rules
+     */
+    static addCSSRules(selector, rules) {
+        const style = this.ensureStyle();
+
+        let body = '';
+        for (const prop in rules) {
+            const cssProp = prop.replace(/[A-Z]/g, m => '-' + m.toLowerCase());
+            body += `${cssProp}: ${rules[prop]};`;
+        }
+
+        style.textContent += `\n${selector}{${body}}`;
+    }
+
+    static removeCSSRules(selector) {
+        const style = this.ensureStyle();
+        const regex = new RegExp(`${selector}\\s*{[^}]*}`, 'g');
+        style.textContent = style.textContent.replace(regex, '');
+    }
+
+    static editCSSRules(selector, rules) {
+        this.removeCSSRules(selector);
+        this.addCSSRules(selector, rules);
+    }
+
+
     /**
      * @param {HTMLElement} element
      * @param {Object[]} options
      * 
-     * @param {string} [options[].label] Display label (defaults to `string([value])`)
-     * @param {*} options[].value The option value
-     * @param {*} [options[].key] Defaults to `option-[index]`
+     * @description
+     * Each option object may contain:
+     * - label: Display label (defaults to `string([value])`)
+     * - value: The option value
+     * - key: Defaults to `option-[index]`
+     * 
+     * events:
+     * - optionchange: Fired when an option is selected or unselected
+     * - optionselect: Fired when an option is selected
+     * - optionunselect: Fired when an option is unselected
+     * 
+     * @param {{label?: string, value: any, key?: string}[]}
      * 
      * @param {{
-     *  defaultText? string
+     *  defaultText?: string,
+     *  dropdownOpenRightText?: string,
+     *  dropdownClosedRightText?: string
      * }} settings
      */
     constructor(element, options, settings = {}) {
+        super();
+        Dropdown.ensureStyle();
+
         this.container = element;
         this.buttonElement = null;
         this.dropdownElement = null;
+        this.dropdownContainerElement = null;
         this.options = options;
 
         this.settings = {
             defaultText: settings.defaultText || 'Select an option',
+            dropdownOpenRightText: settings.dropdownOpenRightText || '▼',
+            dropdownClosedRightText: settings.dropdownClosedRightText || '▼',
         }
 
         const button = document.createElement('button');
@@ -32,7 +136,7 @@ class Dropdown {
             button.classList.toggle('dropdown-hover');
         });
         
-        button.textContent = this.settings.defaultText + " ▼";
+        button.textContent = this.settings.defaultText + " " + this.settings.dropdownClosedRightText;
 
         this.buttonElement = button;
 
@@ -58,6 +162,36 @@ class Dropdown {
             labelElement.htmlFor = optionElement.id;
             labelElement.textContent = option.label || String(option.value);
 
+            labelElement.addEventListener('click', () => {
+                optionElement.checked = !optionElement.checked;
+                optionElement.dispatchEvent(new Event('change'));
+            });
+
+
+            optionElement.addEventListener('change', () => {
+                this.dispatchEvent(new CustomEvent("optionchange", {
+                    detail: {
+                        key: optionElement.dataset.key,
+                        value: option.value,
+                        checked: optionElement.checked,
+                        element: optionElement
+                    },
+                    bubbles: true,
+                }));
+
+                this.dispatchEvent(new CustomEvent(
+                    optionElement.checked ? "optionselect" : "optionunselect",
+                    {
+                        detail: {
+                            key: optionElement.dataset.key,
+                            value: option.value,
+                            element: optionElement
+                        },
+                        bubbles: true,
+                    }
+                ));
+            });
+
             optionContainer.appendChild(optionElement);
             optionContainer.appendChild(labelElement);
 
@@ -71,10 +205,81 @@ class Dropdown {
 
         this.container.appendChild(button);
         this.container.appendChild(optionsContainer);
+        this.dropdownContainerElement = optionsContainer;
 
-        button.addEventListener('click', () => {
+        button.addEventListener('click', (e) => {
             optionsContainer.classList.toggle('hide');
+            button.textContent = this.settings.defaultText + " " + (optionsContainer.classList.contains('hide') ? this.settings.dropdownClosedRightText : this.settings.dropdownOpenRightText);
+
+
             optionsContainer.style.minWidth = `${button.offsetWidth - 16 - 1.5}px`;
         });
+
+        document.body.addEventListener('click', (event) => {
+            if (!this.container.contains(event.target) && !optionsContainer.classList.contains('hide')) {
+                optionsContainer.classList.add('hide');
+            }
+        });
+    }
+
+
+    /**
+     * sets the option with the given key to the given value
+     * @param {boolean} key
+     * @param {boolean} value
+     */
+    set(key, value){
+        const optionElement = this.container.querySelector(`.dropdown-checkbox[data-key="${key}"]`);
+        if(optionElement){
+            optionElement.checked = value;
+            optionElement.dispatchEvent(new Event('change'));
+        }
+    }
+
+    /**
+     * selects the option with the given key
+     * @param {boolean} key 
+     */
+    select(key){
+        this.set(key, true);
+    }
+
+    /**
+     * unselects the option with the given key
+     * @param {boolean} key
+     */
+    unselect(key){
+        this.set(key, false);
+    }
+
+    /**
+     * @returns {{key: string}}
+     */
+    selectedKeys(){
+        const selectedKeys = {};
+        const optionElements = this.container.querySelectorAll('.dropdown-checkbox');
+        optionElements.forEach((optionElement) => {
+            selectedKeys[optionElement.dataset.key] = optionElement.checked ? true: false;
+        });
+        return selectedKeys;
+    }
+
+    /**
+     *
+     * @returns {Array<{key: string, value: *, element: HTMLInputElement}>}
+     */
+    selected(){
+        const selectedOptions = [];
+        const optionElements = this.container.querySelectorAll('.dropdown-checkbox');
+        optionElements.forEach((optionElement) => {
+            if(optionElement.checked){
+                selectedOptions.push({
+                    key: optionElement.dataset.key,
+                    value: optionElement.value,
+                    element: optionElement
+                });
+            }
+        });
+        return selectedOptions;
     }
 }
